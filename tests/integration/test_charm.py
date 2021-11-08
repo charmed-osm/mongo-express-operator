371751549c77c1d9cfde75484cff386f0d5a56b6
Your Name <you@example.com>
2021-11-08 01:07:48 +0100

    Add change-password action

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -7,6 +7,7 @@ import base64
 import logging
 import urllib.request
 from pathlib import Path
+from typing import Tuple
 
 import pytest
 import yaml
@@ -42,18 +43,49 @@ async def test_build_and_deploy(ops_test: OpsTest):
 
 @pytest.mark.abort_on_fail
 async def test_mongo_express_is_up(ops_test: OpsTest):
+    username, password = await get_credentials_action(ops_test)
+    assert username == "admin"
+    await authenticate(ops_test, username, password)
+
+
+@pytest.mark.abort_on_fail
+async def test_mongo_express_change_password(ops_test: OpsTest):
+    username, password_1 = await get_credentials_action(ops_test)
+    new_password = await change_password_action(ops_test)
+    username_2, password_2 = await get_credentials_action(ops_test)
+    assert username == username_2
+    assert password_1 != new_password
+    assert new_password == password_2
+    await authenticate(ops_test, username, new_password)
+
+
+async def get_credentials_action(ops_test: OpsTest) -> Tuple[str, str]:
+    """Execute get-credentials action and return (username, password)."""
     action = (
         await ops_test.model.applications["mongo-express"].units[0].run_action("get-credentials")
     )
     output = await ops_test.model.wait_for_action(action.entity_id)
     assert output.status == "completed"
-    assert "username" in output.results and output.results["username"] == "admin"
+    assert "username" in output.results
     assert "password" in output.results
-    username = output.results["username"]
-    password = output.results["password"]
+    return output.results["username"], output.results["password"]
+
+
+async def change_password_action(ops_test: OpsTest) -> str:
+    """Execute change-password action and return new-password."""
+    action = (
+        await ops_test.model.applications["mongo-express"].units[0].run_action("change-password")
+    )
+    output = await ops_test.model.wait_for_action(action.entity_id)
+    assert output.status == "completed"
+    assert "new-password" in output.results
+    return output.results["new-password"]
+
+
+async def authenticate(ops_test: OpsTest, username: str, password: str) -> None:
+    """Authenticate using username and password."""
     status = await ops_test.model.get_status()
     address = status["applications"]["mongo-express"]["units"]["mongo-express/0"]["address"]
-
     url = f"http://{address}:8081"
     logger.info("mongo-express public address: http://%s", url)
     token = base64_encode(f"{username}:{password}")
